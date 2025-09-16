e8604d3147022cc85cd646fd7dd788c5115c040b
uBongi <136004037+Bongiiii@users.noreply.github.com>
2025-09-16 15:51:42 -0400

    Increase timeout for backend request to 60 minutes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,7 +92,7 @@ function App() {
 
       const response = await axios.post(`${BACKEND_URL}/extract`, formData, {
         responseType: "blob",
-        timeout: 1200000, // 20 minutes timeout
+        timeout: 3600000, // 60 minutes timeout
         headers: {
           "Content-Type": "multipart/form-data",
         },
